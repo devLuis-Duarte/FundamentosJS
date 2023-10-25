2be40adb1228fa2c1a4451a26fd8729e1235bd02
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2023-10-25 11:46:15 -0300

    Tipos de variaveis no javascript

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ console.log('Hello World')
 console.error("Este é um erro")
 console.warn("Este é um aviso")
 console.log("Esta é uma mensagem")
+
+//variáveis
+
+var x = 10
+//maneiras mais modernas
+let y = 15 //valor alteravel
+const z = 20 //valor constante
+
+if(1){
+    let y = 55
+    console.log(y)
+}
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+y = 25
+
+console.log(y)
