f8a91a2770dbd25ce97bca41c4f4ee933c451012
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2023-10-25 12:01:01 -0300

    Tipos de dados no javascript

--- script.js
+++ script.js
@@ -25,3 +25,40 @@ console.log(z)
 y = 25
 
 console.log(y)
+
+//tipos de dados
+const name = "Joao"
+console.log(name)
+console.log(typeof name)
+
+const shirtsQty = 4
+console.log(shirtsQty)
+console.log(typeof shirtsQty)
+
+const decimal = 4.15
+console.log(decimal)
+console.log(typeof decimal)
+
+const isApproved = false //true ou false
+console.log(isApproved)
+console.log(typeof isApproved)
+
+let surname = null
+console.log(surname)
+console.log(typeof surname)
+
+surname = "Saldanha"
+console.log(surname)
+console.log(typeof surname)
+
+let age
+console.log(age)
+console.log(typeof age)
+
+const languages = ['Javascript', 'PHP', 'C#']
+console.log(languages)
+console.log(typeof languages)
+
+const user = {email: "email@gmail.com", password: "1234", age: 28} //object literals
+console.log(user)
+console.log(typeof user)
