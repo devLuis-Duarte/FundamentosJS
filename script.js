19d151c879bcd5b78338aba862540f749a6cd06d
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2023-10-30 19:04:34 -0300

    criacao de objetos, atributos e destructuring

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const fullname = "Lukas Saldanha"
 
 console.log(fullname.length)
 
-const stringToArray = fullname.split()
+const stringToArray = fullname.split() //transforma string em array
 
 console.log(stringToArray)
 
@@ -94,7 +94,7 @@ console.log(list)
 
 console.log(list[list.length - 1]) //como pega o ultimo elemento da lista
 
-list.push('g')
+list.push('g')//adiciona um novo elemento ao final da lista
 
 console.log(list)
 
@@ -109,3 +109,64 @@ console.log(list)
 list.unshift('a')//adiciona um novo elemento no comeco da lista
 
 console.log(list)
+
+const nome = 'Luis Saldanha'
+const idade = 18
+
+// console.log("Olá meu nome eh " + nome + "e minha idade eh " + idade)
+
+// let lista = ['maca', 'pera', 'banana', 'morango']
+
+// for(let i = 0; i<lista.length; i++){
+//     console.log(lista[i])
+// }
+
+// lista.push('kiwi')
+
+// console.log(lista)
+
+// lista.pop()
+
+// console.log(lista)
+
+// lista.unshift('kiwi')
+
+// console.log(lista)
+
+// lista.shift()
+
+// console.log(lista)
+
+//objetos
+const product = {
+    name: "Camisa",
+    price: 15.99,
+    inStock: true,
+    sizes: ['P', 'M', 'G'],
+    'Main color': 'Azul',
+}
+
+console.log(product.name)
+
+console.log(product['name'])
+
+console.log(product['Main color'])
+
+//destructuring
+
+const{price, inStock} = product
+            //^
+            //|
+            //|
+            //|
+            //mesma coisa, forma simplificada de acessar os atributos de um objeto
+/*const price = product.price
+const inStock = product.inStock*/
+
+console.log(price)
+console.log(inStock)
+
+const [n1, n2] = list
+
+console.log(n1)
+console.log(n2)
